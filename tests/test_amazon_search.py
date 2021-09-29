aec66dbf08351c749381de669a06b7ae6390e2e2
Yusra <alimyyusra@gmail.com>
2021-09-29 19:47:03 -0400

    amazon_basic navigation created.

--- tests/test_amazon_search.py
+++ tests/test_amazon_search.py
@@ -5,4 +5,4 @@ def test_amazon_search(browser):
     browser.get("https://www.amazon.com/")
     browser.find_element_by_id("twotabsearchtextbox").send_keys("Hello World")
     browser.find_element_by_id("nav-search-submit-button").click()
-    assert browser.title()
+    assert browser.title == "Amazon.com. Spend less. Smile more."
